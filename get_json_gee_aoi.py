1997b92381adb698d21d6b3eca865ec976af2251
Gee Araya <GeeAraya@users.noreply.github.com>
2020-08-27 22:57:59 -0400

    create code
    load a json file to generate a gee aoi

---
+++ get_json_gee_aoi.py
@@ -0,0 +1,9 @@
+#python code to read a geojson file and convert it to an ee geometry obeject for AOI filters
+workingpath = 'G:\My Drive\Geog656\lab9\EIT.json'
+json_geo_df = geopandas.read_file(workingpath)
+json_geo_js = json_geo_df.to_json()
+json_geo_js_dict = json.loads(json_geo_js)
+for feature in json_geo_js_dict['features']:
+    json_geo = feature['geometry']['coordinates']
+area = ee.Geometry.Polygon (json_geo)
+print(area)
